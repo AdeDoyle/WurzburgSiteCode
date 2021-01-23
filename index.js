0affbe5515611e79bf141aec5fc2f9e7ca231830
admd9 <admd911@hotmail.com>
2021-01-23 12:19:07 +0000

    Tokens and POS tags now displayed for each gloss.

--- index.js
+++ index.js
@@ -47,21 +47,21 @@ function epSidebar(data){
 
 
 // Insert Glosses in body
-function epBody(data){
+function epBody(data) {
 	var epistList = "";
 	var epistCount = 1;
 	var curFo = "start";
-	for (i=0; i<data.length; i++){
+	for (i=0; i<data.length; i++) {
 		var epistName = data[i].epistle;
 		var setId = "<h2 id='epist" + epistCount + "'>" + epistName + "</h2>";
 		epistList += setId;
 		epistCount++;
 		var fols = data[i].folios;
-		for (j=0; j<fols.length; j++){
+		for (j=0; j<fols.length; j++) {
 			var folName = fols[j].folio;
 			var folIdSplit = folName.split(" ");
 			var checkFo = folIdSplit.join("")
-			if (!(checkFo == curFo)){
+			if (!(checkFo == curFo)) {
 				curFo = checkFo;
 				var folId = "<h3 id='fol" + checkFo + ".1'>" + folName + "</h3>";
 			}else{
@@ -69,7 +69,7 @@ function epBody(data){
 			}
 			epistList += folId;
 			var glosses = fols[j].glosses;
-			for (k=0; k<glosses.length; k++){
+			for (k=0; k<glosses.length; k++) {
 				var glossHand = glosses[k].glossHand;
 				var glossNum = glosses[k].glossNo;
 				var glossTxt = glosses[k].glossText;
@@ -86,9 +86,10 @@ function epBody(data){
 				var lemEnd = parseInt(lemPos) + lemLen;
 				var blem = "<strong><u>" + lem + "</u></strong>";
 				var latLem = lat.substr(0, lemPos) + blem + lat.substr(lemEnd);
+				var tokens = glosses[k].glossTokens2;
 				epistList += "<ul id='" + glid + "' class='gluaiseannabileoige'><li class='uimhir'>" + glossNum + ".</li><li class='anghluais'>" + glossTxt + "</li></ul>";
 				epistList += "<div id='" + glid + "info' class='boscafaisneise'>";
-				if (glossFNs.includes("<sup>")){
+				if (glossFNs.includes("<sup>")) {
 					epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Gloss:</li><li class='line'>" + glossFNs + "</li></ul>";
 				}
 				epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Translation:</li><li class='line'>" + glossTrans + "</li></ul>";
@@ -97,20 +98,45 @@ function epBody(data){
 				epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Verse Info:</li><li class='line'>" + lineNum + ".</li></ul>";
 				epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Verse Latin:</li><li class='anlaidin'><em>" + latLem + "</em></li></ul>";
 				var fns = glosses[k].footnotes;
-				if (fns){
+				if (fns) {
 					epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Footnotes: </li></ul>";
-					for (l=0; l<fns.length; l++){
+					for (l=0; l<fns.length; l++) {
 						var fn = fns[l];
-						if (l==0){
+						if (l==0) {
 							epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>" + fn.substr(0, 1) + ".</li><li class='line'>" + fn.substr(2) + "</li></ul>";
-						}else if (l>0){
+						} else if (l>0) {
 							epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>" + fn.substr(0, 1) + ".</li><li class='line'>" + fn.substr(2) + "</li></ul>";
 						}
 					}
 				}
 				if (glossANs) {
-					epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'><br></li><li class='line'blah><br></li></ul>";
+					epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'><br></li><li class='line'><br></li></ul>";
 					epistList += "<ul class='faisneisgluaiseanna'><li class='faisneis'>Site Notes:</li><li class='line'>" + glossANs + "</li></ul>";
+				}
+				epistList += "<ul class='faisneisfocail'><li class='faisneis'></li><li class='toktitle'><br></li><li class='toktitle'><br></li><li class='toktitle'><br></li></ul>";
+				epistList += "<ul class='faisneisfocail'><li class='faisneis'></li><li class='toktitle'>Token</li><li class='toktitle'>Headword</li><li class='toktitle'>Part-of-speech</li></ul>";
+				epistList += "<ul class='faisneisfocail'><li class='faisneis'></li><li class='toktitle'><br></li><li class='toktitle'><br></li><li class='toktitle'><br></li></ul>";
+				for (l=0; l<tokens.length; l++) {
+					var token = tokens[l];
+					var tok_inst = token[0];
+					var tok_head = token[2];
+					var tok_pos = token[1];
+					var tok_num = l + 1
+					if (tok_pos == "<Latin>") {
+						tok_inst = "<em>" + tok_inst + "</em>";
+						tok_head = "";
+						tok_pos = "<strong>*Latin word</strong>";
+					} else if (tok_pos == "<Latin CCONJ>") {
+						tok_inst = "<em>" + tok_inst + "</em>";
+						tok_head = "";
+						tok_pos = "<strong>*Latin word</strong>";
+					} else if (tok_pos == "<unknown>") {
+						tok_pos = "<strong>*missing POS</strong>";
+					}
+					if (tok_head == "<unknown>") {
+						tok_head = "<strong>*missing headword</strong>";
+					}
+					epistList += "<ul class='faisneisfocail'><li class='faisneis'>" + tok_num + ".</li><li class='tok'>" + tok_inst + "</li><li class='head'>" + tok_head + "</li><li class='pos'>" + tok_pos + "</li></ul>";
 				}
 				epistList += "</div>";
 			}
